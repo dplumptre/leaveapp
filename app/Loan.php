c84de840f2238391a1037472890631a1c8177f09
Plumptre <dplumptre@yahoo.com>
2018-04-19 13:53:25 +0100

    demmy

---
+++ app/Loan.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+protected $guarded = ['id'];
+
+protected  $table = 'loans';
+
+class Loan extends Model
+{
+    
+
+
+    public function users()
+    {
+        return $this->hasMany('App\User','user_id','id');
+ 
+    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+
+
+
+
+
+}
